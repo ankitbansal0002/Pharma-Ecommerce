4e93c20b2ba64d064b0db2e0665678c7744dd24d
Ankit Bansal <ankitbansal0002@gmail.com>
2024-05-12 23:16:55 +0530

    delete User & Auth layer Added

--- Pharma-Ecommerce Backend/dist/routes/user.js
+++ Pharma-Ecommerce Backend/dist/routes/user.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { newUser, getAllUsers, getUser } from "../controllers/user.js";
+import { newUser, getAllUsers, getUser, deleteUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 const app = express.Router();
 // route - localhost/api/v1/user/new
 app.post("/new", newUser);
 // route - localhost/api/v1/all
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 // route - localhost/api/v1/dynamic { it should be below new and all other custom can create issue}
 app.get("/:id", getUser);
+app.delete("/:id", adminOnly, deleteUser);
+// app.route(":id").get(getUser).delete(deleteUser);
 export default app;
