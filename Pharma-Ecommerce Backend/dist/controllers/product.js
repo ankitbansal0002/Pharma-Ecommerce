b87c6d0cb63f7c082c2c29b6b31c0a98ab581f24
Ankit Bansal <ankitbansal0002@gmail.com>
2024-05-16 00:54:57 +0530

    Adding more controllers to Product
    Delete Product, Update Product, admin-all-product, get single product, get all categories, get latest Products

--- Pharma-Ecommerce Backend/dist/controllers/product.js
+++ Pharma-Ecommerce Backend/dist/controllers/product.js
@@ -25,3 +25,74 @@ export const newProduct = TryCatch(async (req, res, next) => {
         message: "Product Created Successfully",
     });
 });
+export const getlatestProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+// Revalidate on New,Update,Delete Product & on New Order
+export const getAllCategories = TryCatch(async (req, res, next) => {
+    const categories = await Product.distinct("category");
+    return res.status(200).json({
+        success: true,
+        categories,
+    });
+});
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product Not Found", 404));
+    return res.status(200).json({
+        success: true,
+        product,
+    });
+});
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+    const product = await Product.findById(id);
+    if (!product)
+        return next(new ErrorHandler("Invalid Product Id", 404));
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("Old Photo Deleted");
+        });
+        product.photo = photo.path;
+    }
+    if (name)
+        product.name = name;
+    if (price)
+        product.price = price;
+    if (stock)
+        product.stock = stock;
+    if (category)
+        product.category = category;
+    await product.save();
+    return res.status(201).json({
+        success: true,
+        message: "Product Updated Successfully",
+    });
+});
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product not found", 404));
+    rm(product.photo, () => {
+        console.log("Product photo deleted");
+    });
+    await product.deleteOne();
+    return res.status(201).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+});
--- Pharma-Ecommerce Backend/src/routes/products.ts
+++ Pharma-Ecommerce Backend/src/routes/products.ts
@@ -1,30 +1,30 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { newProduct } from "../controllers/product.js";
+import { newProduct , getAllCategories,getlatestProducts, getAdminProducts, getSingleProduct,deleteProduct,updateProduct} from "../controllers/product.js";
 
 const app = express.Router();
 
 //To Create New Product  - /api/v1/product/new
-app.post("/new", singleUpload, newProduct);
+app.post("/new", adminOnly, singleUpload, newProduct);
 
 //To get all Products with filters  - /api/v1/product/all
 // app.get("/all", getAllProducts);
 
 // //To get last 10 Products  - /api/v1/product/latest
-// app.get("/latest", getlatestProducts);
+app.get("/latest", getlatestProducts);
 
 // //To get all unique Categories  - /api/v1/product/categories
-// app.get("/categories", getAllCategories);
+app.get("/categories", getAllCategories);
 
 // //To get all Products   - /api/v1/product/admin-products
-// app.get("/admin-products", adminOnly, getAdminProducts);
+app.get("/admin-products", adminOnly, getAdminProducts);
 
 // To get, update, delete Product
-// app
-//   .route("/:id")
-//   .get(getSingleProduct)
-//   .put(adminOnly, singleUpload, updateProduct)
-//   .delete(adminOnly, deleteProduct);
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(adminOnly, singleUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
 
 export default app;
